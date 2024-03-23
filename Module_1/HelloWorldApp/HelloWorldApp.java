e79aa34240cd5ab3f2c570073cbab28743fc487d
perreaultjessi <163781359+perreaultjessi@users.noreply.github.com>
2024-03-22 23:45:07 -0700

    Update HelloWorldApp.java
    Changes committed by Git Bash via IntelliJ IDE were not reflected, update necessary

--- Module_1/HelloWorldApp/HelloWorldApp.java
+++ Module_1/HelloWorldApp/HelloWorldApp.java
@@ -1,3 +1,12 @@
+/*Citation Liang, (2019). Introduction to Java programming and Data Structures: Comprehensive version (17th ed.). Pearson Education inc.*/
 public class HelloWorldApp {
+    public static void main(String[] args) {
+        String firstName = "Jessi";
+        String lastName = "Perreault";
+        String message = "Hello World from %s %s.";
+
+        System.out.printf(message, firstName, lastName);
+
+    }
 
 }
