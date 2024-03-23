c6f475585f440899894c49978ba7e147390329e7
perreaultjessi <163781359+perreaultjessi@users.noreply.github.com>
2024-03-22 23:43:50 -0700

    Update TestFanApp.java
    Changes commited via GitBash were not reflected of IntelliJ IDE code, Update needed

--- Module_1/HelloWorldApp/TestFanApp.java
+++ Module_1/HelloWorldApp/TestFanApp.java
@@ -1,3 +1,23 @@
+/*Citation Liang, (2019). Introduction to Java programming and Data Structures: Comprehensive version (17th ed.). Pearson Education inc.*/
+//Define the TestFanApp Class
 public class TestFanApp {
+    //Method for entry of program
+    public static void main(String[] args) {
+        //Create Default Fan
+        Fan defaultFan = new Fan();
+        
+        //Create Custom Fan
+        //The fan is set to medium speed, is turned on, has a radius of 8.0, and is colored blue
+        Fan argumentFan = new Fan(Fan.MEDIUM, true, 8.0, "blue");
 
+        //Display Default Fan variables
+        System.out.println("Default Fan: ");
+        System.out.println(defaultFan);
+
+        //Display Custom Fan Variables
+        System.out.println("\nArgument Fan: ");
+        System.out.println(argumentFan);
+    }
 }
+
+
