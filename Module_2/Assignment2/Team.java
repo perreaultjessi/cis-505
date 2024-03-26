5bd65952bd2cbe5ff4ef3d288693c2b23115e36a
Jessi <jperreault@my365.bellevue.edu>
2024-03-25 18:02:44 -0700

    Moved JAR PerreaultAssignment2.jar file to Module_2 folder

--- Module_2/Assignment2/Team.java
+++ Module_2/Assignment2/Team.java
@@ -1,2 +1,41 @@
-package Assignment2;public class Team {
+/*Citation Liang, (2019). Introduction to Java programming and Data Structures: Comprehensive version (17th ed.). Pearson Education inc.*/
+/*Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved*/
+/*Payne, D., (2021). CSD 405 Intermediate Java Programming. Bellevue University, all rights reserved.*/package Assignment2;
+
+public class Team {
+    private String teamName; //Team name input
+    private String[] players; //Players assigned to a team
+    private int playerCount; //Number of players on a team
+
+    //Constructor to create a team
+    public Team(String teamName) {
+        this.teamName = teamName;
+        this.players = new String[20]; //default size of players set to 20
+        this.playerCount = 0; //default state no players
+    }
+
+    //getMethod to add players
+    public void addPlayer(String playerName) {
+        if (playerCount < players.length) { //checking for space
+            players[playerCount] = playerName; //adding new player
+            playerCount++; //player count
+        } else {
+            System.out.println("Cannot add more players. 20 player limit.");
+        }
+    }
+
+    //getMethod to return players
+    public String[] getPlayers() {
+        return players;
+    }
+
+    //getMethod to return player count
+    public int getPlayerCount() {
+        return playerCount;
+    }
+
+    //getMethod to return team name
+    public String getTeamName() {
+        return teamName;
+    }
 }
--- Module_2/Assignment2/TestSportsTeamApp.java
+++ Module_2/Assignment2/TestSportsTeamApp.java
@@ -1,2 +1,60 @@
-package Assignment2;public class TestSportsTeamApp {
+/*Citation Liang, (2019). Introduction to Java programming and Data Structures: Comprehensive version (17th ed.). Pearson Education inc.*/
+/*Krasso, R., (2021). CIS 505 Intermediate Java Programming. Bellevue University, all rights reserved*/
+/*Payne, D., (2021). CSD 405 Intermediate Java Programming. Bellevue University, all rights reserved.*/
+
+package Assignment2;
+
+import java.util.Scanner;
+
+public class TestSportsTeamApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean continueInput = true;
+
+        //Welcome message
+        System.out.println("Welcome to the Sports Team App");
+
+        while (continueInput) {
+            //User input for team name
+            System.out.print("Enter the team name: ");
+            String teamName = scanner.nextLine();
+
+            //Creation of new team object
+            Team team = new Team(teamName);
+
+            //User input for player names
+            System.out.print("Enter player names separated by commas and no spaces: ");
+            String playerNames = scanner.nextLine();
+            String[] players = playerNames.split(",");
+
+            //Add players to the team
+            for (String player : players) {
+                team.addPlayer(player);
+            }
+
+            //Display team summary message
+            System.out.println("--Team Summary--");
+
+            //Display message for number of players
+            System.out.println("Number of players in team " + team.getTeamName() + ": " + team.getPlayerCount());
+            //Display message for player names
+            System.out.print("Players on team " + team.getTeamName() + ": ");
+            for (int i = 0; i < team.getPlayerCount(); i++) {
+                System.out.print(team.getPlayers()[i]); //display player names
+                if (i < team.getPlayerCount() - 1) {
+                    System.out.print(", "); //adds comma except for last player name
+                }
+            }
+            System.out.println(); //next line
+
+            //Display message to either continur running program or end
+            System.out.print("Do you want to continue? (y/n): ");
+            String choice = scanner.nextLine();
+            continueInput = choice.equalsIgnoreCase("yes");
+        }
+        scanner.close(); //close scanner
+
+        //Display end of line message when the user enters n
+        System.out.println("End of line...");
+    }
 }
